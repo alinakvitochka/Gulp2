defdff6eecd04d6b39919591fa28bcbc79b45e34
Alina O <alinapiatakova1309@gmail.com>
2025-04-23 11:59:59 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const beautify = require('gulp-cssbeautify');
+
+async function loadAutoprefixer() {
+    const autoprefixer = (await import('gulp-autoprefixer')).default;
+    return autoprefixer;
+}
+
+async function style() {
+    const autoprefixer = await loadAutoprefixer();
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(beautify())
+        .pipe(gulp.dest('./assets/css')) 
+        .pipe(browserSync.stream());
+}
+
+function minifyCSS() {
+    return gulp.src('./css/**/*.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./scss/**/*.scss', style); 
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.minifyCSS = minifyCSS;
+exports.watch = watch;
